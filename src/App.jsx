09483897870b12221493e5e4fc947dfda2a1a8a0
Akmalov-dev <amirbekdebruyne@gmail.com>
2026-06-27 03:08:22 +0500

    new style of registering

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import cof from '../src/images/cod1.jpg'
+import cof from '../src/images/cod2.jpg'
 
 function App() {
 
