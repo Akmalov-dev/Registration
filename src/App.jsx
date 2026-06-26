52b5d67b14a93b0896d729c5d87dbee5d4313a5d
Akmalov-dev <amirbekdebruyne@gmail.com>
2026-06-26 18:12:24 +0500

    new picture

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import cof from '../src/images/cod1.jpg'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <div>
-          <input type="text" placeholder='email'/>
-          <input type="text" placeholder='phone number'/>
+      <img className='cod' src={cof} alt="" />
+      <div className='boss'>
+        <div className='inputs'>
+          <input type="text" placeholder='Name' />
+          <input type="text" placeholder='Email'/>
+          <input type="text" placeholder='Password'/>
+          <input type="text" placeholder='Phone'/>
           <button>Submit</button>
         </div>
       </div>
